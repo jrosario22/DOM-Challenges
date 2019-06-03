f12c14ca06d46096035ef830bbbbef85a84d3e0b
Jeson Rosario <jesonrosario22@gmail.com>
2019-06-03 19:35:11 -0400

    Fixed login header change

--- js/passwordCheck.js
+++ js/passwordCheck.js
@@ -1,12 +1,17 @@
 
-let passwordCheck = document.getElementById("login-submit");
-passwordCheck.addEventListener("click", ()=> {
-    var x = document.getElementById("password").value;
-    if(x != 12345678){
-        alert("Password entered is incorrect");
-    } else if(x == 12345678) {
-        document.getElementById("header1").innerText = "Successful log in";
-    }
+let passwordCheck = document.getElementById("form");
+passwordCheck.addEventListener("submit", function(event){
+    passwordCheckFunc();
+    event.preventDefault();
 });
 
-passwordCheck();
+function passwordCheckFunc() { 
+        var x = document.getElementById("password").value;
+        if(x != 12345678){
+            alert("Password entered is incorrect");
+        } else if(x == 12345678) {
+            document.getElementById("header1").innerText = "Successful log in";
+        }
+}
+
+
